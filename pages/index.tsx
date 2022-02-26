cd1ed71544b7ef69210c50f85e6d163bece5157e
Maick Souza <maick_a_s@msn.com>
2022-02-26 13:47:26 -0300

    styles(web): Home #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <div>maick souza</div>;
+  return (
+    <>
+      <Head>
+        <title>nearmaick.dev.br | Início</title>
+        <meta name='description' content='Bem vindo ao meu portal | blog' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <section>
+        <div>
+          <img src='https://github.com/nearmaick.png' alt='Maick Souza Foto' />
+        </div>
+
+        <div>
+          <h3>Olá, sou maick souza</h3>
+          <span>JavaScript FullStack Developer</span>
+          <p>
+            Uma pessoa disciplinada que sempre busca sua melhor versão amanhã.
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
